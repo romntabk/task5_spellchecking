1e1a7247ac4936bc7e308d91536fe0f2f52327fd
Roman <rnbatyrkaev@edu.hse.ru>
2019-12-05 02:24:28 +0500

    try

--- src/dictionary.h
+++ src/dictionary.h
@@ -2,6 +2,7 @@
 #define  _DICTIONARY_H_
 
 #include  <iostream>
+#include  <fstream>
 #include  <vector>
 #include  <list>
 #include  <algorithm>
@@ -18,9 +19,12 @@ class hash_function
 public:
 
     unsigned int operator()( const string& s )  const {
-        
-        
-        // Complete definition
+        //http://rsdn.org/forum/alg/360746.all
+		int hash = 0;
+		int i = 0;
+		for(size_t i=0;i<s.length();i++)
+			hash = (hash << 5) + hash + s[i];
+		return hash;
         
     }
 };
@@ -40,6 +44,6 @@ class Dictionary: public HashSet<string, hash_function, equality> {
     // Complete definition
 public:
 	Dictionary(string filename);
-
+	Dictionary() {}
 };
 #endif // _DICTIONARY_H_
